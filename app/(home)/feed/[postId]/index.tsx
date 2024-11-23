c00a80ad1b5f8f4808a94480a10ee13a22df79c2
nataliaoliveto <nataliaoliveto94@gmail.com>
2024-11-23 15:41:22 -0300

    feat: add post details

--- app/(home)/feed/[postId]/index.tsx
+++ app/(home)/feed/[postId]/index.tsx
@@ -1,4 +1,4 @@
-import { Link, useLocalSearchParams, useRouter } from "expo-router";
+import { Link, Redirect, useLocalSearchParams, useRouter } from "expo-router";
 import { View, ScrollView } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import {
@@ -7,6 +7,8 @@ import {
   Button,
   Title,
   Text,
+  Card,
+  Avatar,
 } from "react-native-paper";
 import { usePostById } from "@hooks/usePost";
 import React, { useEffect, useState } from "react";
@@ -14,34 +16,45 @@ import { useAppTheme } from "src/theme";
 import { useMaterialProductList } from "@hooks/useMaterialProduct";
 import { IMAGE } from "@constants/image.constant";
 import axios from "axios";
+import { useAuth } from "@clerk/clerk-expo";
 
 export default function DetailPost() {
+  const { userId, isSignedIn } = useAuth();
+  if (!userId || !isSignedIn) return <Redirect href="/(auth)/sign-in" />;
   const params = useLocalSearchParams();
   const postId = params.postId as string;
   const { data: post, isPending } = usePostById({ id: postId });
   const theme = useAppTheme();
   const { data: materials } = useMaterialProductList();
-  const imageUrl = `${IMAGE.CLOUDINARY_URL}${IMAGE.POST_FOLDER}/${postId}.jpg?timestamp=${Date.now()}`;
-  const [image, setImage] = useState<string>("");
+  const imageUrlPost = `${IMAGE.CLOUDINARY_URL}${IMAGE.POST_FOLDER}/${postId}.jpg?timestamp=${Date.now()}`;
+  const imageUrlPostMaterial = `${IMAGE.CLOUDINARY_URL}${IMAGE.UTILS_FOLDER}/${post?.materialProductId}.png`;
+  const imageUrlPostUser = `${IMAGE.CLOUDINARY_URL}${IMAGE.USER_GENERIC}`;
+  const [imagePost, setImagePost] = useState<string>(imageUrlPostMaterial);
+  const [imagePostUser, setImagePostUser] = useState<string>(imageUrlPostUser);
   const router = useRouter();
 
   useEffect(() => {
     (async () => {
       try {
-        const test = await axios.get(imageUrl);
-        if (test.status === 200) setImage(imageUrl);
+        const test = await axios.get(imageUrlPost);
+        if (test.status === 200) setImagePost(imageUrlPost);
         return;
       } catch (e) {
         if (post) {
-          setImage(
-            `${IMAGE.CLOUDINARY_URL}${IMAGE.UTILS_FOLDER}/${post?.materialProductId}.png`
-          );
+          setImagePost(imageUrlPostMaterial);
         } else {
-          setImage(`${IMAGE.CLOUDINARY_URL}${IMAGE.UTILS_FOLDER}/special.png`);
+          setImagePost(
+            `${IMAGE.CLOUDINARY_URL}${IMAGE.UTILS_FOLDER}/special.png`
+          );
         }
         return;
       }
     })();
+    if (post?.userId) {
+      const timestamp = `?timestamp=${Date.now()}`;
+      const urlImage = `${IMAGE.CLOUDINARY_URL}${IMAGE.USER_FOLDER}/${post.userId}.jpg${timestamp}`;
+      setImagePostUser(urlImage);
+    }
   }, [post]);
 
   return (
@@ -56,46 +69,103 @@ export default function DetailPost() {
       <ScrollView contentContainerStyle={{ flexGrow: 1, padding: 16 }}>
         {isPending && <ActivityIndicator size="large" />}
         <View style={{ width: "100%" }}>
-          <Text>
-            Soy {post?.username} y
-            {post?.purpouse == "HAVE" ? " ofrezco " : " busco "}
-            {materials
-              ?.find((material) => material.id === post?.materialProductId)
-              ?.name.toLowerCase() || "material"}
-          </Text>
-          <Text>Descripción: {post?.description}</Text>
-
-          <Text>Está reservado: {post?.isReserved ? "Sí" : "No"}</Text>
+          <Card style={{ alignItems: "center", marginVertical: 20 }}>
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                padding: 10,
+              }}
+            >
+              <Avatar.Image
+                size={64}
+                style={{ marginRight: 10 }}
+                source={{
+                  uri: imagePostUser,
+                }}
+              />
+              <View style={{ flex: 1 }}>
+                <Text
+                  style={{
+                    fontSize: 20,
+                    fontWeight: "700",
+                    color: theme.colors.backdrop,
+                  }}
+                >
+                  @{post?.username}
+                </Text>
+                <Text
+                  style={{
+                    marginTop: 5,
+                    fontSize: 16,
+                    color: post?.isReserved
+                      ? theme.colors.error
+                      : theme.colors.secondary,
+                  }}
+                >
+                  {post?.isActive
+                    ? "Publicación activa"
+                    : "Publicación finalizada"}{" "}
+                  {post?.isReserved ? "y reservada" : "y disponible"}
+                </Text>
+              </View>
+            </View>
+            <Card.Content>
+              <Text
+                variant="titleLarge"
+                style={{ color: theme.colors.onBackground }}
+              >
+                {post?.purpouse == "HAVE" ? "Ofrezco " : "Busco "}
+                {materials
+                  ?.find((material) => material.id === post?.materialProductId)
+                  ?.name.toLowerCase() || "material"}
+                {", unidades: "}
+                {post?.quantity}
+              </Text>
+              <Text
+                variant="bodyMedium"
+                style={{ marginVertical: 10, color: theme.colors.onBackground }}
+              >
+                {post?.description}
+              </Text>
+            </Card.Content>
+            <Card.Cover
+              source={{ uri: imagePost }}
+              style={{
+                margin: 10,
+                width: 350,
+                height: 250,
+                borderRadius: 8,
+              }}
+            />
+            <Card.Actions style={{ marginBottom: 10 }}>
+              {post?.userId == userId && (
+                <Button
+                  mode="contained"
+                  onPress={() => router.push(`/feed/${postId}/edit`)}
+                  loading={isPending}
+                  disabled={isPending}
+                  buttonColor={theme.colors.secondaryContainer}
+                  textColor={theme.colors.onSecondaryContainer}
+                >
+                  Editar publicación
+                </Button>
+              )}
+              <Button
+                mode="contained"
+                onPress={() => console.log("comentario")}
+                loading={isPending}
+                disabled={isPending}
+                buttonColor={theme.colors.tertiaryContainer}
+                textColor={theme.colors.onTertiaryContainer}
+              >
+                Dejar un comentario
+              </Button>
+            </Card.Actions>
+          </Card>
         </View>
-        <View style={{ flex: 1 }} />
-        <View style={{ marginBottom: 20 }}>
-          <View
-            style={{
-              margin: 10,
-              gap: 15,
-            }}
-          >
-            <Button
-              mode="contained"
-              onPress={() => router.push(`/feed/${postId}/edit`)}
-              loading={isPending}
-              disabled={isPending}
-              buttonColor={theme.colors.secondaryContainer}
-              textColor={theme.colors.onSecondaryContainer}
-            >
-              Editar publicación
-            </Button>
-            <Button
-              mode="contained"
-              onPress={() => console.log("comentario")}
-              loading={isPending}
-              disabled={isPending}
-              buttonColor={theme.colors.tertiaryContainer}
-              textColor={theme.colors.onTertiaryContainer}
-            >
-              Dejar un comentario
-            </Button>
-          </View>
+        <View style={{ margin: 20 }}>
+          <Text>Comentarios</Text>
         </View>
       </ScrollView>
     </SafeAreaView>

7e65d58f7666ed0b362b29496030d88b03a28074
Rodrigo Toyama <toyama.rodrigo@gmail.com>
2024-11-25 13:56:55 -0300

    fix format

--- app/(home)/locations/_layout.tsx
+++ app/(home)/locations/_layout.tsx
@@ -1,15 +1,15 @@
 import { Stack } from "expo-router";
 
-
-export default function LocationsLayout() { 
+export default function LocationsLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" 
+      <Stack.Screen
+        name="index"
         options={{
           title: "Locations",
           headerShown: false,
         }}
       />
     </Stack>
-  )
+  );
 }
